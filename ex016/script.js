1ee90e5fca35c010607551d960416b5c42e28a0e
leonardowecker <leowecker185@gmail.com>
2024-09-08 15:17:41 -0300

    sim

---
+++ ex016/script.js
@@ -0,0 +1,8 @@
+function contar(){
+    var inicio =  document.getElementById('inicio').value
+    var fim =  document.getElementById('fim').value
+    var passo =  document.getElementById('passo').value
+    for(inicio<fim;inicio+passo){
+        
+    } 
+}
